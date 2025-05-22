ee979bd80119e88f0a60911b21bb4433fab7029d
rakeshgm <rakeshgm@redhat.com>
2025-05-22 17:01:04 +0530

    rename key VRClassScheduleKey
    since this key is now being used for
    VGRC and VRC, it makes sense to have a
    common name. The key is renamed to
    ReplicationClassScheduleKey
    
    Signed-off-by: rakeshgm <rakeshgm@redhat.com>

--- internal/controller/volumereplicationgroup_controller.go
+++ internal/controller/volumereplicationgroup_controller.go
@@ -538,8 +538,8 @@ const (
 	// Maintenance mode label
 	MModesLabel = "ramendr.openshift.io/maintenancemodes"
 
-	// VolumeReplicationClass schedule parameter key
-	VRClassScheduleKey = "schedulingInterval"
+	// VolumeReplicationClass and VolumeGroupReplicationClass schedule parameter key
+	ReplicationClassScheduleKey = "schedulingInterval"
 )
 
 func (v *VRGInstance) requeue() {
